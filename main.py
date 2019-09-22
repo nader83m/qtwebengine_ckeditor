f7563ca4c302ba79dadd446a9a257a0822f96275
“nader83m” <naderebrahimpour@gmail.com>
2019-09-22 21:05:56 +0300

    It is provided set a multiline string to CKEditor content.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = QApplication([])
 import qt5reactor
 qt5reactor.install()
 from twisted.internet import defer, reactor
+import os
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -12,33 +13,31 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('mainWindow.ui', self)
-        url = QUrl("file:///media/nadir/data/scripts/qtwebengine_ckeditor/index.html")
+        self.showMaximized()
+
+        # self.showFullScreen()
+        self.path = os.getcwd()
+        url = QUrl("file://%s/index.html" % self.path)
         self.browser.page().load(url)
         self.btnSaveToFile.clicked.connect(self.btnsavedtofile)
         self.btnLoadFromFile.clicked.connect(self.btnloadfromfile)
 
     def btnsavedtofile(self):
         def callbackfunction(re):
-            print(re)
+            with open('%s/files/out.html' % self.path, 'w') as f:
+                s = f.write(re)
         print(self.browser.page().runJavaScript("CKEDITOR.instances.editor.getData();", callbackfunction))
-
         # self.browser.page().runJavaScript("CKEDITOR.instances.editor.resize( '100%', '350', true );", callbackfunction)
-        print('its work')
 
     def btnloadfromfile(self):
         def callbackfunction(re):
-            print(re)
-        s = ''   
-        with open('/media/nadir/data/scripts/qtwebengine_ckeditor/files/banka_fisi.html', 'r') as f:
+            pass
+        s = ''
+        with open('%s/files/input.html' % self.path, 'r') as f:
             s = f.read()
 
-        print(s)
-            
-
-        self.browser.page().runJavaScript("CKEDITOR.instances.editor.setData('%s');" % s, callbackfunction)
+        self.browser.page().runJavaScript("CKEDITOR.instances.editor.setData(`%s`);" % s, callbackfunction)
  
-        print('its work')
-
 if __name__ == "__main__":
     view = MainWindow()
     view.show()
